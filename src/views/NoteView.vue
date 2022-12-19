7c9c46307a9687fd31986a9e0a3bfeebacaeb2d5
Amir Hallaj <lastprogamer0@gmail.com>
2022-12-19 19:02:03 +0330

    copy and share note added.

---
+++ src/views/NoteView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import { useNoteStore } from "../stores/note";
+import router from "@/router";
+
+const { note } = storeToRefs(useNoteStore());
+
+if (note.value.id == null) {
+    router.push({ name: "read" });
+}
+
+const handleCopy = async () => {
+    await navigator.clipboard.writeText(note.value.content);
+};
+const handleShare = async () => {
+    await navigator.clipboard.writeText(
+        `${window.location.origin}/read/${note.value.id}`
+    );
+};
+</script>
+
+<template>
+    <h2 class="mt-4 text-center text-xl font-bold text-accent">
+        {{ note.title }}
+    </h2>
+    <p class="max-w-[320px] break-words p-8 text-center text-xl text-neutral">
+        {{ note.content }}
+    </p>
+    <div class="flex w-[320px] flex-wrap justify-evenly pt-5">
+        <button
+            @click="handleCopy"
+            class="text-md h-12 rounded-lg border-2 border-secondary px-4 text-[goldenrod]"
+        >
+            Copy Content
+        </button>
+        <button
+            @click="handleShare"
+            class="text-md h-12 rounded-lg border-2 border-secondary px-4 text-[goldenrod]"
+        >
+            Share Link
+        </button>
+    </div>
+</template>
